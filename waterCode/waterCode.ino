4d9382826b306771d2757549eaa25d2a6819bd93
ry6969 <harrycanalita17@gmail.com>
2025-11-22 13:14:07 +0800

    added 90 degree return to waterCode

--- waterCode/waterCode.ino
+++ waterCode/waterCode.ino
@@ -1,8 +1,15 @@
 #define PUMP_RELAY_PIN 2
+#define SERVO2_PIN 6
+
+#include <Servo.h>
+
+Servo servo2;
 
 void setup() {
   pinMode(PUMP_RELAY_PIN, OUTPUT);
   digitalWrite(PUMP_RELAY_PIN, HIGH); // relay OFF initially (assuming LOW = ON)
+  servo2.attach(SERVO2_PIN);
+  servo2.write(90); // reset
   Serial.begin(9600);
   Serial.println("Enter 1 to turn ON the pump, 0 to turn OFF:");
 }
